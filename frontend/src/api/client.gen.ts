ad87af4e42c3f37ca5a01e9b22dba8536729db4f
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2025-09-26 22:37:07 +0700

    fix: host client

--- frontend/src/api/client.gen.ts
+++ frontend/src/api/client.gen.ts
@@ -22,7 +22,7 @@ export type CreateClientConfig<T extends ClientOptions = ClientOptions2> = (
 
 export const client = createClient(
   createConfig<ClientOptions2>({
-    baseURL: 'http://localhost:5001',
+    baseURL: 'https://api.ifest.fauzanazz.com',
     withCredentials: true,
   })
 );
